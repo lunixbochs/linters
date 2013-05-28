1dda827583ef62b141ef7cb58b955c00550b9789
Ryan Hileman <lunixbochs@gmail.com>
2013-05-27 23:02:15 -0700

    add embedded JS linter

--- javascript.py
+++ javascript.py
@@ -4,3 +4,7 @@ class JavaScript(Linter):
     language = 'javascript'
     cmd = ('jsl', '-stdin')
     regex = r'^\((?P<line>\d+)\):\s+(?P<error>.+)'
+
+class EmbeddedJS(JavaScript):
+    language = 'html'
+    selector = 'source.js.embedded.html'
